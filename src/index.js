3f76e0c77fd29c982dbb991a65a1c99ebaf746f5
Daniel Miller <daniel.miller.prs@gmail.com>
2021-02-27 13:50:30 +0100

    feat: problem partially solved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack = [];
+  strarray = str.split('');
+
+  for (i = 0; i < strarray.length; i++) {
+    for (j = 0; j < bracketsConfig.length; j++) {
+      if (strarray[i] == bracketsConfig[j][0] && bracketsConfig[j][0] != bracketsConfig[j][1]) {
+      stack.push(strarray[i]);
+      break;
+      }
+
+      if (strarray[i] == bracketsConfig[j][1] && stack[stack.length - 1] == bracketsConfig[j][0]) {
+        stack.pop();
+        break;
+      }
+    }
+  }
+  return stack.length == 0;
 }
